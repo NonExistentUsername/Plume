89c8010e3b4835c88b9f2a5d295c71273ec298ef
Dima <mayor4090@gmail.com>
2022-04-13 17:21:25 +0300

    Main class update

--- src/main/java/com/unknown/plumedesktop/PlumeApplication.java
+++ src/main/java/com/unknown/plumedesktop/PlumeApplication.java
@@ -1,18 +1,33 @@
 package com.unknown.plumedesktop;
 
+import com.unknown.plumedesktop.tdcontroller.AuthUpdateHandler;
+import com.unknown.plumedesktop.tdcontroller.TDParams;
+import com.unknown.plumedesktop.tdcontroller.TelegramClient;
+import com.unknown.plumedesktop.tdcontroller.UpdateHandler;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class PlumeApplication extends Application {
+    private static Stage stage = null;
+    private static AuthSceneLoader loginScenesLoader = null;
+    private static TelegramClient tc = null;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PlumeApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage _stage) throws IOException {
+        loginScenesLoader = new AuthSceneLoader(true);
+        stage = _stage;
+        stage.setTitle("Plume");
+        stage.setScene(loginScenesLoader.getStartScene());
+        stage.show();
+
+        tc = new TelegramClient();
+        tc.create(new UpdateHandler(new AuthUpdateHandler(tc)), new TDParams(false));
+    }
+
+    static public void changeScene(Scene scene) {
         stage.setScene(scene);
         stage.show();
     }
